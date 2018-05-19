9c8ba74da843f5c02cfea75922e118c93e634d7b
srappaport <sarah.rappaport@greendot.org>
2018-05-19 18:59:07 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,38 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".") == true
+      return true
+    else return false
+    end
   end
 
   def question?
-
+    if self.end_with?("?") == true
+      return true
+    else return false
+    end
   end
 
   def exclamation?
-
+    if self.end_with?("!") == true
+      return true
+    else return false
+    end
   end
 
   def count_sentences
 
+    if self.include? ","
+      arr= self.gsub! "!", "."
+      arr = arr.gsub "?", "."
+      #remove empty strings from arr
+
+      arr=arr.split(".")
+      arr= arr.reject{|e| e.empty?}
+      num=arr.count
+    else
+      num=(self.split).count
+    end
   end
 end
