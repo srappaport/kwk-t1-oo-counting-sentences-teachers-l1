dcdf276c1478aec2660a3b0cf8905f12a474c300
srappaport <sarah.rappaport@greendot.org>
2018-05-19 17:35:33 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -12,12 +12,14 @@ class String
   def question?
     if self.end_with?("?") == true
       return true
+    else return false
     end
   end
 
   def exclamation?
     if self.end_with?("!") == true
       return true
+    else return false
     end
   end
 
