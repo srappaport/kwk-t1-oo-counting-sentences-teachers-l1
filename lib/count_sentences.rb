3622b834c2af7c9cad62ff9a63587980b70588e8
srappaport <sarah.rappaport@greendot.org>
2018-05-19 17:34:37 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -5,17 +5,19 @@ class String
   def sentence?
     if self.end_with?(".") == true
       return true
-    else
-      return false
     end
   end
 
   def question?
-
+    if self.end_with?("?") == true
+      return true
+    end
   end
 
   def exclamation?
-
+    if self.end_with?("!") == true
+      return true
+    end
   end
 
   def count_sentences
